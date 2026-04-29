49c76e867b066efbebef7cda6c6a1228c1d8ad14
Piyush Jha <piyushjha910@gmail.com>
2026-04-29 07:36:47 +0530

    added a new txt file

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get("/", (req, res) => {
 
 // Route 2 → second PDF (optional)
 app.get("/file2", (req, res) => {
-    const filePath = path.join(__dirname, "dsp2.pdf");
+    const filePath = path.join(__dirname, "rm.txt");
     res.download(filePath);
 });
 
